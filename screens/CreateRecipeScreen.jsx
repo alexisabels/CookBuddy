5ff75376a456f3968a5d20979f5966c5b38ad1bf
alexisabels <isabelsanchezalex@gmail.com>
2024-07-22 02:11:13 +0200

    Add recipe & snackbar feedback

--- screens/CreateRecipeScreen.jsx
+++ screens/CreateRecipeScreen.jsx
@@ -1,17 +1,43 @@
 import React, { useState } from "react";
 import {
   View,
-  Text,
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
+  ScrollView,
 } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-import { Appbar, TextInput } from "react-native-paper";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Appbar, Button, TextInput, Snackbar } from "react-native-paper";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const CrearRecetaScreen = () => {
   const [nombre, setNombre] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [snackbarText, setSnackbarText] = useState("");
+
+  const guardarReceta = async () => {
+    Keyboard.dismiss();
+    try {
+      const receta = { id: Date.now().toString(), nombre };
+      const recetasExistentes = await AsyncStorage.getItem("recetas");
+      const recetasActualizadas = recetasExistentes
+        ? JSON.parse(recetasExistentes)
+        : [];
+      recetasActualizadas.push(receta);
+      await AsyncStorage.setItem(
+        "recetas",
+        JSON.stringify(recetasActualizadas)
+      );
+      setSnackbarText("Receta guardada con éxito!");
+      setVisible(true);
+      setNombre("");
+    } catch (error) {
+      setSnackbarText("Error al guardar la receta.");
+      setVisible(true);
+      console.error("Error al guardar la receta", error);
+    }
+  };
+
+  const onDismissSnackBar = () => setVisible(false);
 
   return (
     <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
@@ -27,9 +53,13 @@ const CrearRecetaScreen = () => {
               onChangeText={setNombre}
               mode="outlined"
             />
+            <Button onPress={guardarReceta}>Guardar</Button>
           </View>
         </ScrollView>
       </TouchableWithoutFeedback>
+      <Snackbar visible={visible} onDismiss={onDismissSnackBar} duration={3000}>
+        {snackbarText}
+      </Snackbar>
     </KeyboardAvoidingView>
   );
 };
