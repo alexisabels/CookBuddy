45d190c175e4cac8f26ea669b2d78a9408ddab1b
alexisabels <isabelsanchezalex@gmail.com>
2024-07-22 02:01:22 +0200

    recipe list example

--- screens/MyRecipesScreen.jsx
+++ screens/MyRecipesScreen.jsx
@@ -1,16 +1,46 @@
 // En CrearRecetaScreen.js
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
-import { Appbar } from "react-native-paper";
+import { FlatList } from "react-native-gesture-handler";
+import { Appbar, Card } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const MyRecipesScreen = () => {
+  const [recetas, setRecetas] = useState([
+    {
+      id: "1",
+      titulo: "Tarta de manzana",
+      ingredientes: "Manzanas, azúcar, harina",
+      pasos: "Cortar manzanas...",
+    },
+    {
+      id: "2",
+      titulo: "Paella",
+      ingredientes: "Arroz, pollo, mariscos",
+      pasos: "Cocinar el arroz...",
+    },
+  ]);
+
+  const RecipeCard = ({ item }) => (
+    <Card style={{ margin: 8 }}>
+      <Card.Title title={item.titulo} />
+      <Card.Content>
+        <Text>{item.ingredientes}</Text>
+        <Text>{item.pasos}</Text>
+      </Card.Content>
+    </Card>
+  );
   return (
     <>
       <Appbar.Header>
-        <Appbar.Content title="Crear Receta" />
+        <Appbar.Content title="Mis Recetas" />
       </Appbar.Header>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hola, esta es la pantalla para ver tus recetas.</Text>
+      <View>
+        <FlatList
+          data={recetas}
+          renderItem={RecipeCard}
+          keyExtractor={(item) => item.id}
+        />
       </View>
     </>
   );
